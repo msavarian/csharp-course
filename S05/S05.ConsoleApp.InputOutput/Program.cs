1ac62d4fd3a8d18aadb28d54928a142fdeeec1db
msavarian <sav68.net@gmail.com>
2023-10-22 21:04:57 +0330

    S05 Completed

---
+++ S05/S05.ConsoleApp.InputOutput/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Write("Please Enter Your Name: ");
+Console.WriteLine("Hi " + Console.ReadLine());
+
+
+Console.Write("Hasan Enter Your Age: ");
+Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
