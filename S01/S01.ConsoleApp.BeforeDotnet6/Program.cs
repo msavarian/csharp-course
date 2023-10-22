31ce5b2e16d2cb9a4292988160c9300e2aea6201
msavarian <sav68.net@gmail.com>
2023-10-22 20:04:02 +0330

    S05 Completed

---
+++ S01/S01.ConsoleApp.BeforeDotnet6/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace S01.ConsoleApp.BeforeDotnet6
+{
+
+    internal class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ S01/S01.ConsoleApp.TopLevelStatement/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Title = "C# Course S01";
+
+// Console.WindowHeight = 30;
+
+
+
+// inline comment - change the color
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Hello, C#!");
+
+
+/*
+ * multiline comment
+ * test
+ * comment
+ * ...
+ */
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Hello, C# Again!");
+
+Console.ForegroundColor = ConsoleColor.White;
