dd493a99b9f99e212ee77070bb8d460e7b02c35d
msavarian <sav68.net@gmail.com>
2023-10-23 02:39:58 +0330

    S07 (Naming)

---
+++ S07/S07.ConsoleApp.Naming/Program.cs
@@ -0,0 +1,35 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+// ***** Naming Rules in C# ***************************
+
+// * Acceptable Variable Names By C# Rules:
+int myNumber;
+int My_Number;
+int _myNumber;
+int number1;
+
+// * Not Acceptable Variable Names By C# Rules:
+// int 12345; // Error
+// int 1number; // Error
+// int myNumber$; // Error
+// int 12345; // Error
+// int my number; // Error
+// int public; //Error
+
+
+
+
+// ***** Naming Conventions in C# ***************************
+// camelCase
+// good naming
+int t=30;               // Too Short
+int temp=30;            // Temperature or temporal
+int temperature=30;     // good
+int roomTemp=30;        // it's better
+int roomTemerature=30;  // perhaps it's too long
+
+int employeeNumber=1;
+int empNumber = 1;
+
+int ssn;
