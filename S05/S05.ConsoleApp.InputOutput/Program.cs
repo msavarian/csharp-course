d5e0a486cb426dda643a315b6646cf4c8a9cd0e1
msavarian <sav68.net@gmail.com>
2023-10-22 21:20:03 +0330

    add \n

--- S05/S05.ConsoleApp.InputOutput/Program.cs
+++ S05/S05.ConsoleApp.InputOutput/Program.cs
@@ -6,7 +6,11 @@ Console.WriteLine("Hi " + Console.ReadLine());
 Console.Write("Hasan Enter Your Age: ");
 Console.ReadLine();
 
-Console.WriteLine();
-Console.WriteLine();
-Console.WriteLine();
-Console.WriteLine();
+
+Console.WriteLine("\n\n\n");
+
+//   \n\n\n make a new empty line in console
+
+//Console.WriteLine();
+//Console.WriteLine();
+//Console.WriteLine();
